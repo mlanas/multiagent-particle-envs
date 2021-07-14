d463fbb0ffb168a98ada79ceef4e7ebc502c5a00
Manuel Lañas <manuel.lanas@outlook.com>
2021-07-14 16:52:01 +0200

    variable n_agents in simple_spread

--- multiagent/scenarios/simple_spread.py
+++ multiagent/scenarios/simple_spread.py
@@ -4,12 +4,13 @@ from multiagent.scenario import BaseScenario
 
 
 class Scenario(BaseScenario):
-    def make_world(self):
+    def make_world(self, n_agents=3):
+        assert n_agents > 1, "Number of agents must be at least 2!"
         world = World()
         # set any world properties first
         world.dim_c = 2
-        num_agents = 3
-        num_landmarks = 3
+        num_agents = n_agents
+        num_landmarks = n_agents
         world.collaborative = True
         # add agents
         world.agents = [Agent() for i in range(num_agents)]
